4057ada093e0c699b9216c09d64355ef8b9ae358
hamza-salihovic <130321555+hamza-salihovic@users.noreply.github.com>
2025-06-29 10:56:44 +0000

    deleted the tokens

--- Dockerfile
+++ Dockerfile
@@ -105,11 +105,11 @@ RUN bash -c "$(curl -fsSL https://raw.github.com/xicodomingues/francinette/maste
 RUN sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || true
 RUN echo "source $HOME/.oh-my-zsh/oh-my-zsh.sh" >> $HOME/.zshrc
 
-# Add these lines to your Dockerfile
+# remove line:112 and line:113 if linux is 64bit
 RUN cd /tmp && \
     git clone https://github.com/cacharle/c_formatter_42 && \
     cd c_formatter_42 && \
-    mv /data/clang-format-linux /data/clang-format-linux_64bit && \
+    mv /data/clang-format-linux /data/clang-format-linux_64bit && \ 
     mv /data/clang-format-darwin-arm64 /data/clang-format-linux && \
     pip3 install -e .
 
--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 mkdir -p .devcontainer
 
-curl -fsSL https://raw.githubusercontent.com/hamza-salihovic/docker_setup/refs/heads/main/Dockerfile?token=GHSAT0AAAAAAC5FHS45U65IHQEOQNEU4YNQ2DBCKMA -o .devcontainer/Dockerfile
-curl -fsSL https://raw.githubusercontent.com/hamza-salihovic/docker_setup/refs/heads/main/devcontainer.json?token=GHSAT0AAAAAAC5FHS45FWTUV35BS7YRIV242DBCK4Q -o .devcontainer/devcontainer.json
-curl -fsSL https://raw.githubusercontent.com/hamza-salihovic/docker_setup/refs/heads/main/setup-gdb.sh?token=GHSAT0AAAAAAC5FHS45D3CZ6NQYQAXAHBLS2DBCTTA -o .devcontainer/setup-gdb.sh
+curl -fsSL https://raw.githubusercontent.com/hamza-salihovic/docker_setup/refs/heads/main/Dockerfile -o .devcontainer/Dockerfile
+curl -fsSL https://raw.githubusercontent.com/hamza-salihovic/docker_setup/refs/heads/main/devcontainer.json -o .devcontainer/devcontainer.json
+curl -fsSL https://raw.githubusercontent.com/hamza-salihovic/docker_setup/refs/heads/main/setup-gdb.sh -o .devcontainer/setup-gdb.sh
