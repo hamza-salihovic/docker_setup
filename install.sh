fa8e7b6df96a9c23291dddc9d55390a8be2c13ed
hamza-salihovic <130321555+hamza-salihovic@users.noreply.github.com>
2025-06-29 11:28:16 +0200

    Update install.sh
    added the script for gdb

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ mkdir -p .devcontainer
 
 curl -fsSL https://raw.githubusercontent.com/hamza-salihovic/docker_setup/refs/heads/main/Dockerfile?token=GHSAT0AAAAAAC5FHS45U65IHQEOQNEU4YNQ2DBCKMA -o .devcontainer/Dockerfile
 curl -fsSL https://raw.githubusercontent.com/hamza-salihovic/docker_setup/refs/heads/main/devcontainer.json?token=GHSAT0AAAAAAC5FHS45FWTUV35BS7YRIV242DBCK4Q -o .devcontainer/devcontainer.json
+curl -fsSL https://raw.githubusercontent.com/hamza-salihovic/docker_setup/refs/heads/main/setup-gdb.sh?token=GHSAT0AAAAAAC5FHS45D3CZ6NQYQAXAHBLS2DBCTTA -o .devcontainer/setup-gdb.sh
