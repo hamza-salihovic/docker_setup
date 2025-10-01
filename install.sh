4c30286e110a722dc11d20f61fd2c7758a6ecedc
hamza-salihovic <hamzasalihovic@gmail.com>
2025-10-01 12:14:52 +0200

    new setup if something is wrong go back to e15d9d8b2d4ee809a389242a464d1f7f18b69188

--- install.sh
+++ install.sh
@@ -3,6 +3,4 @@
 mkdir -p .devcontainer
 
 curl -fsSL https://raw.githubusercontent.com/hamza-salihovic/docker_setup/refs/heads/main/devcontainer.json -o .devcontainer/devcontainer.json
-curl -fsSL https://raw.githubusercontent.com/hamza-salihovic/docker_setup/refs/heads/main/setup-gdb.sh -o .devcontainer/setup-gdb.sh
-curl -fsSL https://raw.githubusercontent.com/hamza-salihovic/docker_setup/refs/heads/main/setup-shell.sh -o .devcontainer/setup-shell.sh
 curl -fsSL https://raw.githubusercontent.com/hamza-salihovic/docker_setup/refs/heads/main/GDB_Help.md -o .devcontainer/GDB_Help.md
